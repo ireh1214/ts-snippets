f0dd6aa08b8d7465e88255c14556a29662054885
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-04-17 18:07:58 +0900

    [수정] noForEach추가, 코드 간결화

--- LV0/test4.ts
+++ LV0/test4.ts
@@ -2,9 +2,7 @@
 // flag에 따라 다른 값 반환하기
 
 export default function solution(a: number, b: number, flag: boolean) {
-	let answer = 0;
-
-	answer = flag === true ? a + b : a - b;
+	const answer = flag === true ? a + b : a - b;
 
 	return answer;
 }
