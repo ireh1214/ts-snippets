482930673bf9772914469c7564257c76f224c2ad
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-04-26 16:59:40 +0900

    Refactoring

--- LV0/test3.ts
+++ LV0/test3.ts
@@ -2,7 +2,7 @@
 //개미 군단
 
 //%, /이 둘다 필요한거같습니다
-function solution(hp: number) {
+export default function solution(hp: number) {
 	const a = hp % 5;
 	const x = Math.floor(hp / 5);
 	const y = Math.floor(a / 3);
