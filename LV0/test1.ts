18199b141222e0d84246dc7a46989471ab7cd962
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-05-03 19:08:30 +0900

    [수정] 5주차 코드 수정 -3

--- LV0/test1.ts
+++ LV0/test1.ts
@@ -7,6 +7,10 @@ export default function solution([a, b, c]: [number, number, number]) {
 	// return max < a + b + c - max ? 1 : 2;
 
 	//구조분해 할당을 이용하기
-	[a, b] = [a, b + c];
-	return a < b ? 1 : 2;
+	const sortX = [a, b, c].sort((x, y) => x - y);
+
+	const [max, x, y] = sortX;
+	const sumXY = x + y;
+
+	return max < sumXY ? 1 : 2;
 }
