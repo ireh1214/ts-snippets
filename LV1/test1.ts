d703d92c38c4219a08e85e58f0aa41a8b4391b59
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-05-21 15:15:02 +0900

    [수정] 8주차 코드 수정 -3

--- LV1/test1.ts
+++ LV1/test1.ts
@@ -1,17 +1,12 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/76501
 //음양 더하기
+import _ from "lodash";
 
 function zip<T, U>(arr1: T[], arr2: U[]): [T, U][] {
 	return arr1.map((v, i) => [v, arr2[i]]);
 }
 
-function solution(absolutes: number[], signs: boolean[]) {
+export default function solution(absolutes: number[], signs: boolean[]) {
 	const x: [number, boolean][] = zip(absolutes, signs);
-	let sum = 0;
-
-	x.forEach(([absolute, sign]) => {
-		sum += sign ? absolute : -absolute;
-	});
-
-	return sum;
+	return _.sum(_.map(x, ([absolute, sign]) => (sign ? absolute : -absolute)));
 }
