9048832a5a57d778f61861a22bd718d0178978ec
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-05-24 18:28:59 +0900

    [갱신] 5/24 코딩테스트 백업

--- LV1/test4.ts
+++
@@ -1,7 +0,0 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/12935
-//제일 작은 수 제거하기
-
-export default function solution(arr: number[]) {
-	const answer = arr.filter((n) => n !== Math.min(...arr));
-	return answer.length > 0 ? answer : [-1];
-}
--- LV1/test9.ts
+++
@@ -1,6 +0,0 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/12906
-//같은 숫자는 싫어
-
-export default function solution(arr: number[]) {
-	return arr.filter((x, i) => x !== arr[i - 1]);
-}
