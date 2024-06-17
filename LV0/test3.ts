88ac19982fff878bff8af61a89c8cb056a34d67c
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-06-18 06:08:26 +0900

    [수정] 11주차 수정 - 3

--- LV0/test3.ts
+++ LV0/test3.ts
@@ -2,18 +2,12 @@
 //점의 위치 구하기
 
 export default function solution(dot: number[]) {
-	const x = dot[0];
-	const y = dot[1];
+	const [x, y] = dot;
 
-	let location = 1; //초기값
-
-	if (x < 0 && y > 0) {
-		location = 2;
-	} else if (x < 0 && y < 0) {
-		location = 3;
-	} else if (x > 0 && y < 0) {
-		location = 4;
+	if (x > 0) {
+		return y > 0 ? 1 : 4;
+		// biome-ignore lint/style/noUselessElse: <explanation>
+	} else {
+		return y > 0 ? 2 : 3;
 	}
-
-	return location;
 }
