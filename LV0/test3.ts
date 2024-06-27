d1f8a44374432d3075da9106cfdd929106483754
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-06-27 16:28:00 +0900

    [수정] 11주차 수정 - 4

--- LV0/test3.ts
+++ LV0/test3.ts
@@ -4,10 +4,5 @@
 export default function solution(dot: number[]) {
 	const [x, y] = dot;
 
-	if (x > 0) {
-		return y > 0 ? 1 : 4;
-		// biome-ignore lint/style/noUselessElse: <explanation>
-	} else {
-		return y > 0 ? 2 : 3;
-	}
+	return x > 0 ? (y > 0 ? 1 : 4) : y > 0 ? 2 : 3;
 }
