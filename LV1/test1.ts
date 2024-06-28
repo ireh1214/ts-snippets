9803b7b1f23d11b4d759dabf84141bef39e0b576
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-06-28 18:48:09 +0900

    [갱신]6/27~28 코딩테스트 백업

--- LV1/test1.ts
+++ LV1/test1.ts
@@ -1,33 +1,23 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/134240
-//푸드 파이트 대회
+//https://school.programmers.co.kr/learn/courses/30/lessons/17681
+//2018 KAKAO BLIND RECRUITMENT [1차] 비밀지도
 
-export default function solution(food: number[]) {
-	//1차 통과
-	// let answer = "";
-	// let set = 0;
-	// let newFood = food.map(String);
+export default function solution(n: number, arr1: number[], arr2: number[]) {
+	const [x, y] = [arr1, arr2].map((arr) =>
+		arr.map((v) => v.toString(2).padStart(n, "0")),
+	);
+	const result = [];
 
-	//     for (const i of newFood){
-	//         const x = (i % 2 !== 0) ? i - 1 : i
-
-	//         if(i === 1){
-	//             set++
-	//         }else {
-	//             answer += (String(set).repeat(x / 2))
-	//             set++
-	//         }
-
-	//     }
-
-	// return answer + "0" + [...answer].reverse().join("");
-
-	//2차 (리팩토링)
-	let answer = "";
-
-	for (let set = 1; set < food.length; set++) {
-		answer += String(set).repeat(Math.floor(food[set] / 2));
+	for (const [i, val] of x.entries()) {
+		let hint = "";
+		for (let j = 0; j < n; j++) {
+			if (val[j] === "1" || y[i][j] === "1") {
+				hint += "#";
+			} else {
+				hint += " ";
+			}
+		}
+		result.push(hint);
 	}
 
-	// biome-ignore lint/style/useTemplate: <explanation>
-	return answer + "0" + [...answer].reverse().join("");
+	return result;
 }
