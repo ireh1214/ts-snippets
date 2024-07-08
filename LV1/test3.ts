c74dc1817882bf9387bf680cf34f017c74d4187d
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-07-08 18:00:05 +0900

    [수정]14주차 수정 - 2

--- LV1/test3.ts
+++ LV1/test3.ts
@@ -1,17 +1,20 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/138477
 //명예의 전당(1)
 
-function solution(k: number, score: number[]) {
-	//1차 풀이
-	const hof = []; //hallOfFame
+export default function solution(k: number, score: number[]) {
+	const hof = [];
 	const answer = [];
 
 	for (const i of score) {
 		hof.push(i);
+		hof.sort((a, b) => b - a);
+
 		if (hof.length > k) {
-			hof.splice(hof.indexOf(Math.min(...hof)), 1);
+			hof.pop();
 		}
-		answer.push(Math.min(...hof));
+
+		answer.push(hof[hof.length - 1]);
 	}
+
 	return answer;
 }
